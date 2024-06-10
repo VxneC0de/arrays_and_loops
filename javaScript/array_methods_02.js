936d457b58c52a4895b1847210297986e395cb03
VxneC0de <vanerubio2020@gmail.com>
2024-06-10 16:10:11 -0400

    feat: start repository, finished arrays_01 and array_methods_02

---
+++ javaScript/array_methods_02.js
@@ -0,0 +1,28 @@
+//Exercise:
+//Use the correct Array method to remove the last item of the fruits array.
+
+/*
+const fruits = ["Banana", "Orange", "Apple"];
+delete fruits[2];
+fruits.pop()
+*/
+
+
+
+//Ejercicio:
+//Use the correct Array method to add "Kiwi" to the fruits array.
+
+/*
+const fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Kiwi");
+*/
+
+
+
+
+//Ejercicio:
+//Use the splice() method to remove "Orange" and "Apple" from fruits.
+
+
+const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.splice(1, 2);
