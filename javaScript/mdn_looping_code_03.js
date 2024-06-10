258a47fa3ee13830a87b4cf77ee8c0fa49207868
VxneC0de <vanerubio2020@gmail.com>
2024-06-10 16:42:16 -0400

    feat: start mdn_looping_code_03

---
+++ javaScript/mdn_looping_code_03.js
@@ -0,0 +1,15 @@
+/*
+Loops 1
+
+In our first looping task we want you to start by creating a simple loop that goes through all the items in the provided myArray and prints them out on the screen inside list items (i.e., <li> elements), which are appended to the provided list.
+*/
+
+const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
+const list = document.createElement('ul');
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+section.appendChild(list);
